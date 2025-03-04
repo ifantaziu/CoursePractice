00ca24d1468d2923e32d02ff08e0a133daa21c10
ifantaziu <roscairina012@gmail.com>
2025-03-04 20:30:11 +0300

    first part of homework

---
+++ src/Lesson4Homework/Variables.java
@@ -0,0 +1,18 @@
+package Lesson4Homework;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        long a = 7811080815L;
+        int b = 55235358;
+        char c = 'A';
+        boolean d = true;
+        double e = 3.14157;
+        System.out.println("Current World Population = " + a);
+        System.out.println("Cars Produced This Year = " + b);
+        System.out.println("First Letter In Alphabet = " + c);
+        System.out.println("Java Is Amazing = " + d);
+        System.out.println("pi =" + e);
+    }
+
+}
