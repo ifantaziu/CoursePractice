997c5c53d9dd13cc73a5ed01142355f38f6e97cd
ifantaziu <roscairina012@gmail.com>
2025-03-06 21:33:36 +0300

    lesson 4 homework using operators

---
+++ src/Lesson4Homework/MinutesConverter.java
@@ -0,0 +1,10 @@
+package Lesson4Homework;
+
+public class MinutesConverter {
+    public static void main (String[] args) {
+        long minutes = 285816352L;
+        double days = (int) minutes/60/24;
+        double years = (int) days/365;
+        System.out.print ("In the " + minutes +" minutes, we have " + days +" days and " + years + " years");
+    }
+}
