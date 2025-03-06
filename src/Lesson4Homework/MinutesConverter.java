fd4accd7dc413b21f98636933d1fad7a647f7ced
ifantaziu <roscairina012@gmail.com>
2025-03-06 21:57:08 +0300

    lesson 4 homework using operators

--- src/Lesson4Homework/MinutesConverter.java
+++ src/Lesson4Homework/MinutesConverter.java
@@ -3,8 +3,8 @@ package Lesson4Homework;
 public class MinutesConverter {
     public static void main(String[] args) {
         long minutes = 285816352L;
-        double days = (int) minutes / 60 / 24;
-        double years = (int) days / 365;
+        double days = (int) minutes / 60 / 24; //intentionat am castuit pe integer, pentru ca sa avem zile complete
+        double years = (int) days / 365; //intentionat am castuit pe integer, pentru ca sa avem anii completi
         System.out.print("In the " + minutes + " minutes, we have " + days + " days and " + years + " years");
     }
 }
